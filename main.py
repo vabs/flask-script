5ac12c8c076ddd6d67b2149d1d67606320b276fc
vabs <njoy.today@gmail.com>
2014-10-10 14:10:44 -0400

    trying a dashboard

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 app.debug = True
 
+#vars used for page
+val = 10
 
 @app.route('/')
 def hello():
+	return render_template('home.html', value = val)
+
+
+@app.route('/listing', methods=['POST'])
+def show_listings():
 	return jsonify({'status': 'ok'})
 
+
+@app.route('/in', methods=['GET'])
+def increment():
+	val += 1
+	return jsonify({'status': 'ok'})	
 
 if __name__ == "__main__":
 	app.run()	
