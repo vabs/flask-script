a733a209514a492c32793fb108ef82c0374b2767
vabs <njoy.today@gmail.com>
2014-10-17 14:58:07 -0400

    merged

--- main.py
+++ main.py
@@ -83,56 +83,6 @@ def get_values():
 	return jsonify(**tokens)
 
 
-@app.route('/listings', methods=['GET', 'POST'])
-def listings_fill():
-	print('listings incoming')
-	raw_str = request.get_data().decode('utf-8')
-	json_obj = json.loads(raw_str)
-	global listings
-	try:
-		listings
-	except NameError:
-		print ("well, it WASN'T defined after all!")
-		listings = []
-		listings.append(json_obj)
-		return jsonify({'status': 'fail'})
-	else:
-		print ("sure, it was defined.")
-		listings.append(json_obj)
-	return jsonify({'status': 'ok'})
-
-
-@app.route('/reviews', methods=['GET', 'POST'])
-def reviews_fill():
-	print('reviews incoming')
-	raw_str = request.get_data().decode('utf-8')
-	json_obj = json.loads(raw_str)
-	global reviews
-	try:
-		reviews
-	except NameError:
-		print ("well, it WASN'T defined after all!")
-		reviews = []
-		reviews.append(json_obj)
-		return jsonify({'status': 'first time'})
-	else:
-		print ("sure, it was defined.")
-		reviews.append(json_obj)
-	return jsonify({'status': 'ok'})
-
-
-@app.route('/getListings')
-def getlisting():
-	global listings
-	return jsonify({'listings': listings})
-
-
-@app.route('/getReviews')
-def getreviews():
-	global reviews
-	return jsonify({'reviews': reviews})
-
-
 @app.errorhandler(404)
 def handle_404(e):
 	return jsonify({'status': '404'})	
