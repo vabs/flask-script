e2013af594ba148da3359a5dcc6cdc795ad3bc9f
vabs <njoy.today@gmail.com>
2014-10-17 17:37:48 -0400

    adding primary key

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ db = SQLAlchemy(app)
 
 class StatsData(db.Model):
 	__tablename__ = 'statsdata'
+	stats_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	token_id = db.Column(db.String)
 	location_name = db.Column(db.String)
 	listing_count = db.Column(db.BigInteger)
