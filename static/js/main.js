d74a5c75f3697cdf7433bad33506ec275c67e6f2
vabs <njoy.today@gmail.com>
2014-10-17 16:18:57 -0400

    Added timing feature

--- static/js/main.js
+++ static/js/main.js
@@ -14,8 +14,8 @@ function getData () {
 		s_table.append('<tr><th>Name</th><th>Listings</th><th>Reviews</th><th>Started At</th><th>Last Updated At</th></tr>');
 		for(var o in data){
 			var b = data[o];
-			var row = '<tr><td>' + b['name'] + '</td><td>' + b['listing'] + '</td><td>' + b['review'] + '</td>
-						<td>' + b['start_time'] + '</td><td>' + b['last_updated_at'] + '</td></tr>';
+			var row = '<tr><td>' + b['name'] + '</td><td>' + b['listing'] + '</td><td>' + b['review'] + 
+					'</td><td>' + b['start_time'] + '</td><td>' + b['last_updated_at'] + '</td></tr>';
 			s_table.append(row);
 		}
 	});
